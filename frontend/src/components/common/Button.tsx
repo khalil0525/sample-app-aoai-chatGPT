ed39b6d5f7822a383e56b3d1701d3c88443ff6e5
Khalil Collins <Collinskhalil@hotmail.com>
2024-11-24 15:11:45 -0500

    Updated SettingsButton to use ref

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -1,5 +1,5 @@
-import { CommandBarButton, DefaultButton, IButtonProps } from '@fluentui/react'
-
+import React, { forwardRef, Ref, useImperativeHandle, useRef } from 'react'
+import { CommandBarButton, DefaultButton, IButton, IButtonProps, IRefObject } from '@fluentui/react'
 import styles from './Button.module.css'
 
 interface ButtonProps extends IButtonProps {
@@ -7,6 +7,28 @@ interface ButtonProps extends IButtonProps {
   text: string | undefined
 }
 
+interface SettingsButtonProps extends IButtonProps {
+  onClick: () => void
+  text: string
+}
+
+export const SettingsButton = forwardRef<CommandBarButton, SettingsButtonProps>((props, ref) => {
+  const { onClick, text, ...restProps } = props
+  const buttonRef = useRef<CommandBarButton>(null)
+
+  useImperativeHandle(ref, () => buttonRef.current as CommandBarButton)
+
+  return (
+    <CommandBarButton
+      ref={buttonRef}
+      className={styles.shareButtonRoot}
+      iconProps={{ iconName: 'Settings' }}
+      onClick={onClick}
+      text={text}
+      {...restProps}
+    />
+  )
+})
 export const ShareButton: React.FC<ButtonProps> = ({ onClick, text }) => {
   return (
     <CommandBarButton
@@ -25,17 +47,6 @@ export const HistoryButton: React.FC<ButtonProps> = ({ onClick, text }) => {
       text={text}
       iconProps={{ iconName: 'History' }}
       onClick={onClick}
-    />
-  )
-}
-
-export const SettingsButton: React.FC<ButtonProps> = ({ onClick, text }) => {
-  return (
-    <CommandBarButton
-      className={styles.settingsButtonRoot}
-      iconProps={{ iconName: 'Settings' }}
-      onClick={onClick}
-      text={text}
     />
   )
 }
